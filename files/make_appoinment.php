877a9ebcce4416b528cbd759ea2bc01f3e6be234
1ayuu <46105942+1ayuu@users.noreply.github.com>
2021-06-01 12:46:29 +0545

    Update make_appoinment.php

--- files/make_appoinment.php
+++ files/make_appoinment.php
@@ -1,8 +1,12 @@
+
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title><script type="text/javascript">
-		function showDoctor() {
+	<title></title>
+	<script type="text/javascript">
+	
+	function showDoctor() {
 			var selectBox = document.getElementById('department');
 			var userInput = selectBox.options[selectBox.selectedIndex].value;
 			if(userInput== 'Paedracition'){
@@ -51,9 +55,22 @@
 		}
 	</script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<p> This page is going to display all the information of the registered patients.</p>
+
+<div class="topnav">
+  <a  href="admin_homepage.php">Home</a> 
+  <a href="./archive.php">Archives</a>
+  <a href="./patients_listview.php"> Patients List</a>
+  <a class="active" href="./new_registration.php">New Registration</a>
+  <a href="./logout.php">Logout</a>
+</div>
+	<p></p>
 	 <!-- <h3>Search using patient's Registration Number</h3> -->
 	 <div class="main-div" >
 	 	
@@ -112,11 +129,21 @@
 	 				?>
 	 			</tbody>
 	 		</table>
-	 		<form action="/action_page.php">
-  <label for="birthdaytime">APPOINTMENT TIME (date and time):</label>
-  <input type="datetime-local" name="AppointmentTime">
- <div>
-		<label>Department</label>
+	<!-- 	<form action="coder.php">
+	 		<div class="form-group mb-3">
+                                <label for="">Patient Id</label>
+                                <input type="text" name="id" class="form-control">
+                            </div>
+                                 <div class="form-group mb-3">
+                                <label for="">Name</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Event Date & Time</label>
+                                <input type="datetime-local" name="event_dt" class="form-control">
+                            </div>
+                         
+	<!--	<label>Department</label>
 		<select name="department" id="department" onchange="return showDoctor();">
 			<option value="Paedracition"> Paedracition </option>
 			<option value="Physician"> Physician </option>
@@ -128,7 +155,7 @@
         <br>
         <div id="select_paedracition" style="visibility: hidden;">
 		<label >Consultant Doctor</label>
-		<select name="consultant_doctor" id="consultant_paedracition">
+		<select name="doctor" id="consultant_paedracition">
 			<option value= "doctor_a"> Dr. Paedratic A </option>
 			<option value="doctor_b"> DR. Paedratic B </option>
 			<option value= "doctor_c"> Dr. Paedratic C</option>
@@ -142,7 +169,7 @@
         <div id="select_physician" style="visibility: hidden;">
         
 		<label >Consultant Doctor</label>
-		<select name="consultant_doctor" id="consultant_physician">
+		<select name="doctor" id="consultant_physician">
 			<option value= "doctor_a"> Dr. A Physician</option>
 			<option value="doctor_b"> DR. B Physician</option>
 			<option value= "doctor_c"> Dr. C Physician</option>
@@ -155,7 +182,7 @@
 	<br>
         <div id="select_gynaecologist" style="visibility: hidden;">
 		<label >Consultant Doctor</label>
-		<select name="consultant_doctor" id="consultant_gynaecologist">
+		<select name="doctor" id="consultant_gynaecologist">
 			<option value= "doctor_a"> Dr. A Paudel gynaecologist</option>
 			<option value="doctor_b"> DR. B Paudel gynaecologist</option>
 			<option value= "doctor_c"> Dr. C Paudel gynaecologist</option>
@@ -168,7 +195,7 @@
 	<br>
         <div id="select_nephrologist" style="visibility: hidden;">
 		<label >Consultant Doctor</label>
-		<select name="consultant_doctor" id="consultant_nephrologist">
+		<select name="doctor" id="consultant_nephrologist">
 			<option value= "doctor_a"> Dr. A Paudel nephrologist</option>
 			<option value="doctor_b"> DR. B Paudel nephrologist</option>
 			<option value= "doctor_c"> Dr. C Paudel nephrologist</option>
@@ -181,7 +208,7 @@
 	<br>
         <div id="select_neurologist" style="visibility: hidden;">
 		<label >Consultant Doctor</label>
-		<select name="consultant_doctor" id="consultant_neurologist">
+		<select name="doctor" id="consultant_neurologist">
 			<option value= "doctor_a"> Dr. A Paudel neurologist</option>
 			<option value="doctor_b"> DR. B Paudel neurologist</option>
 			<option value= "doctor_c"> Dr. C Paudel neurologist</option>
@@ -192,11 +219,12 @@
 		</select>
 	</div>
  <button type="reset">Reset</button>
-		 <button type="submit" class="registration_btn" name="registration_submit">Submit</button>
+		    <div class="form-group mb-3">
+                                <button type="submit" name="save_datetime" class="btn btn-primary">Save Event</button>
+                            </div>
 
 
-
-</form>
+</form>-->
 
 	 		
 	 	</div>
@@ -204,6 +232,170 @@
 	 </div>
 	
 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
+                <?php 
+                    if(isset($_SESSION['status']))
+                    {
+                        ?>
+                            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Hey!</strong> <?php echo $_SESSION['status']; ?>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        <?php
+                         unset($_SESSION['status']);
+                    }
+                ?>
+
+                <div class="card mt-5">
+                    <div class="card-header">
+                        <h4>Appointment to doctor</h4>
+                    </div>
+                    <div class="card-body">
+
+                        <form action="" method="POST">
+                            <div class="form-group mb-3">
+                                <label for="">Patient Name</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Patient Id</label>
+                                <input type="text" name="patient_id" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Event Date & Time</label>
+                                <input type="datetime-local" name="event_dt" class="form-control">
+                            </div>
+                            <div>
+		<label>Department</label>
+		<select name="Department" id="department" onchange="return showDoctor();">
+			<option value="Paedracition"> Paedracition </option>
+			<option value="Physician"> Physician </option>
+			<option value="Gynaecologist"> Gynaecologist </option>
+			<option value="Nephrologist"> Nephrologist</option>
+			<option value="Neurologist"> Neurologist</option>
+		</select>
+	</div>
+        <br>
+        <div id="select_paedracition" style="visibility: hidden;">
+		<label >Consultant Doctor</label>
+		<select name="Consultant_Doctor" id="consultant_paedracition">
+			<option value= "doctor_a"> Dr. Paedratic A </option>
+			<option value="doctor_b"> DR. Paedratic B </option>
+			<option value= "doctor_c"> Dr. Paedratic C</option>
+			<option value="doctor_d"> DR.Paedratic D </option>
+			
+
+			
+		</select>
+	</div>
+	<br>
+        <div id="select_physician" style="visibility: hidden;">
+        
+		<label >Consultant Doctor</label>
+		<select name="Consultant_Doctor" id="consultant_physician">
+			<option value= "doctor_a"> Dr. A Physician</option>
+			<option value="doctor_b"> DR. B Physician</option>
+			<option value= "doctor_c"> Dr. C Physician</option>
+			<option value="doctor_d"> DR. D Physician</option>
+			
+
+			
+		</select>
+	</div>
+	<br>
+        <div id="select_gynaecologist" style="visibility: hidden;">
+		<label >Consultant Doctor</label>
+		<select name="Consultant_Doctor" id="consultant_gynaecologist">
+			<option value= "doctor_a"> Dr. A Paudel gynaecologist</option>
+			<option value="doctor_b"> DR. B Paudel gynaecologist</option>
+			<option value= "doctor_c"> Dr. C Paudel gynaecologist</option>
+			<option value="doctor_d"> DR. D Paudel gynaecologist</option>
+			
+
+			
+		</select>
+	</div>
+	<br>
+        <div id="select_nephrologist" style="visibility: hidden;">
+		<label >Consultant Doctor</label>
+		<select name="Consultant_Doctor" id="consultant_nephrologist">
+			<option value= "doctor_a"> Dr. A Paudel nephrologist</option>
+			<option value="doctor_b"> DR. B Paudel nephrologist</option>
+			<option value= "doctor_c"> Dr. C Paudel nephrologist</option>
+			<option value="doctor_d"> DR. D Paudel nephrologist</option>
+			
+
+			
+		</select>
+	</div>
+	<br>
+        <div id="select_neurologist" style="visibility: hidden;">
+		<label >Consultant Doctor</label>
+		<select name="Consultant_Doctor" id="consultant_neurologist">
+			<option value= "doctor_a"> Dr. A Paudel neurologist</option>
+			<option value="doctor_b"> DR. B Paudel neurologist</option>
+			<option value= "doctor_c"> Dr. C Paudel neurologist</option>
+			<option value="doctor_d"> DR. D Paudel neurologist</option>
+			
+
+			
+		</select>
+	</div>
+	<br>
+
+
+			                           
+                 <div class="form-group mb-3">
+                                <button type="submit" name="save_datetime" class="btn btn-primary">Save </button>
+                      </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+<?php
+$username = "root";
+$password = "";
+$server = 'localhost';
+$db = 'medicalarchiver';
+
+$con = mysqli_connect($server , $username , $password , $db);
+
+
+
+if(isset($_POST['save_datetime']))
+{
+    $name = $_POST['name'];
+    $event_dt = $_POST['event_dt'];
+    $patient_id=$_POST['patient_id'];
+    $Department=$_POST['Department'];
+    $Consultant_Doctor=$_POST['Consultant_Doctor'];
+
+    $query = "INSERT INTO demo (name,eventdt,patient_id,Department,Consultant_Doctor) VALUES ('$name','$event_dt','$patient_id',
+    '$Department',
+    '$Consultant_Doctor')";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $_SESSION['status'] = "appointment successfully";
+      
+    }
+    else
+    {
+        $_SESSION['status'] = "appointment error!";
+  
+    }
+}
+?>
